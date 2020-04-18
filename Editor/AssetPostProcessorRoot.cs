1d31d4ec4f6e4003a15732a610a1c96ccb9a9a88
baba_s <shotababa1206@gmail.com>
2020-04-18 15:50:11 +0900

    fix

---
+++ Editor/AssetPostProcessorRoot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+namespace UniAssetPostprocessorRoot
+{
+	public sealed class AssetPostprocessorRoot : AssetPostprocessor
+	{
+		public delegate void OnPostprocessAllAssetsCallback
+		(
+			string[] importedAssets,
+			string[] deletedAssets,
+			string[] movedAssets,
+			string[] movedFromAssetPaths
+		);
+
+		public static event OnPostprocessAllAssetsCallback OnPostprocessAllAssetsImpl;
+
+		private static void OnPostprocessAllAssets
+		(
+			string[] importedAssets,
+			string[] deletedAssets,
+			string[] movedAssets,
+			string[] movedFromAssetPaths
+		)
+		{
+			OnPostprocessAllAssetsImpl?.Invoke
+			(
+				importedAssets,
+				deletedAssets,
+				movedAssets,
+				movedFromAssetPaths
+			);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Uni Asset Post Processor Root
